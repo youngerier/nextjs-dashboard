8c54c0c7326a9257c204316a26cde34884be92c4
wangping <wangping@a.com>
2024-07-25 16:25:59 +0800

    1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
+  // const revenue = await fetchRevenue();
 
   return (
     <main>
